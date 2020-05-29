ca07296a4501d4894b09e12f84fe4880a333a0c7
GerardoBZentenoM <61177359+GerardoBZentenoM@users.noreply.github.com>
2020-05-28 23:45:07 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,32 @@
+/* #############################################################################
+**    Archivo        : main.dart
+**    Proyecto       : app_aquaponics
+**    Arquitectura   : 
+**    Lenguaje       : Dart 
+**    Plataforma     : Visual Studio Code
+**    SDK            : Flutter 
+**    Versión        : 
+**    Compilador     : 
+**    Fecha/Hora     : 28/05/2020
+**    Descripción    :
+**          
+**         
+**   Componentes     :
+**   Configuraciones : 
+**   Autores         :
+**         Gerardo B. Zenteno M. brallanzenteno@gmail.com
+**          
+**                
+**   Versión        : PreAlpha
+**   Revisión       : A
+**   Release        : 0
+**   Bugs & Fixes   : 
+**   Date           : 
+**   To improve     : 
+**   ¡HUELUM! <3
+**   *en proceso
+** ###########################################################################*/
+
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 
